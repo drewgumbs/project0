20a51c193201d7d0b95b7850d543963b47602516
unknown <alaurich@outlook.com>
2019-11-13 08:34:03 +0100

    changes

---
+++ application.py
@@ -0,0 +1,30 @@
+
+
+from flask import Flask, jsonify, redirect, render_template, request
+
+# Configure application
+app = Flask(__name__)
+
+# Reload templates when they are changed
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+@app.after_request
+def after_request(response):
+    """Disable caching"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/", methods=["GET"])
+def get_index():
+    return redirect("/index")
+
+
+@app.route("/sports", methods=["GET"])
+def get_form():
+    return render_template("sports.html")
+
+
